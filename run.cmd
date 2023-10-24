aea416bbdbadfdb457db522eca8a846ef6ec9239
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-24 08:35:09 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,1 +1,7 @@
 docker network create --subnet=172.20.0.0/16 nagoya-net
+
+docker build -t bind9 .
+docker run -d --rm --name=dns-server --net=nagoya-net --ip=172.20.0.2 bind9
+docker exec -d dns-server /etc/init.d/bind9 start
+
+ping host2.nagoya-foundation.com
